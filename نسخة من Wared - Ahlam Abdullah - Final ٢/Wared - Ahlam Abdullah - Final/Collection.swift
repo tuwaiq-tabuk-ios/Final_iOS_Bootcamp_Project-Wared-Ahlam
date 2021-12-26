77352c39b6f51531115bb369662c97c93ae87a94
ahlam <khald@MacBook-Air-khald.local>
2021-12-26 11:46:57 +0300

    -P-2021_12_26 - partOne

---
+++ نسخة من Wared - Ahlam Abdullah - Final ٢/Wared - Ahlam Abdullah - Final/Collection.swift
@@ -0,0 +1,12 @@
+//
+//  Collection.swift
+//  Wared - Ahlam Abdullah - Final
+//
+//  Created by ahlam  on 16/05/1443 AH.
+//
+
+import Foundation
+import UIKit
+class Collection: UICollectionViewCell {
+  
+}
